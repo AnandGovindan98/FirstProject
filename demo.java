775fe64080097a26a2793a9a2019def306ed6ad0
AnandGovindan98 <anand23.edu@gmail.com>
2024-07-27 19:26:51 +0530

    Switch Multiply

--- demo.java
+++ demo.java
@@ -2,12 +2,9 @@ public class demo
 {
     public static void main(String[]args)
     {
-<<<<<<< HEAD
-        int a=10;
-=======
-        int a=32;
->>>>>>> 76570550328c0a55a11c359aa10b9617f71767cc
+
+        int a=40;
         int b=10;
-        System.out.println(a+b);
+        System.out.println(a*b);
     }
 }
